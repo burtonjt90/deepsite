c90aa8846308ccc5a8a7be2a79ae38f9cb49c34b
burtonjt90 <burtonjt90@gmail.com>
2025-09-27 10:15:00 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./lib/**/*.{js,ts,jsx,tsx,mdx}",
-    "./assets/**/*.{css}"
+    "./assets/**/*.css"
   ],
   theme: {
     container: {
